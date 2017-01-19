8e49277696488d00d4fb1958f35068e741ceb600
Guy Fergusson <fergs@iinet.net.au>
2017-01-20 00:44:55 +0800

    twop pipe segments, random space position

--- love/Bird/main.lua
+++ love/Bird/main.lua
@@ -5,6 +5,9 @@ function love.load()
   playingAreaWidth = 300
   playingAreaHeight = 388
   
+  pipeSpaceHeight = 100
+  pipeSpaceY = love.math.random(0, playingAreaHeight - pipeSpaceHeight)
+  
 end
 
 function love.update(dt)
@@ -15,6 +18,8 @@ function love.update(dt)
     if birdY > 0 then
       birdYSpeed = -165
     end
+    
+    love.load()
   end
   
 end
@@ -26,6 +31,25 @@ function love.draw()
   love.graphics.setColor (224, 214, 68)
   love.graphics.rectangle('fill', 62, birdY, 30, 25)
 
-  love.graphics.setColor(94, 201, 72)
-  love.graphics.rectangle('fill', playingAreaWidth, 0, 54, playingAreaHeight)
+local pipeWidth = 54
+    --local pipeSpaceY = 150
+    --local pipeSpaceHeight = 100
+
+    love.graphics.setColor(94, 201, 72)
+    love.graphics.rectangle(
+        'fill',
+        playingAreaWidth,
+        0,
+        pipeWidth,
+        pipeSpaceY
+    )
+    love.graphics.rectangle(
+        'fill',
+        playingAreaWidth,
+        pipeSpaceY + pipeSpaceHeight,
+        pipeWidth,
+        playingAreaHeight - pipeSpaceY - pipeSpaceHeight
+    )
 end
+
+
